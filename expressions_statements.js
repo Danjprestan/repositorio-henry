2ef04f52d83b379312e60b39b1e20f1d9e7b3a60
Daniel Jimenez <danjprestan@gmail.com>
2022-10-03 20:38:32 -0500

    Práctica de expressions y statements

---
+++ expressions_statements.js
@@ -0,0 +1,45 @@
+//Práctica de expressions y statements.
+
+//TIPOS DE EXPRESIONES
+//Expresiones aritméticas
+1 + 2;
+
+//Expresiones lógicas
+10 > 8;
+
+//Expresiones de String
+"hola" + " Usuario";
+
+//Expresiones primarias (No tienen operadores)
+"HolaMundo";
+45;
+true;
+numero;
+this;
+
+//Expresiones de asignación
+a = 23;
+var x = (4 * 5); //Esto quiere decir que en la variable x se encuentra el valor retornado de 4 * 5
+var y = (c = 2); //Esto quiere decir que en la variable y se encuentra el valor retornado de c = 2
+
+//Expresiones con side effects. ESTOS ESTAN RAROS.
+contador++ ; //Retorna el valor del contador e incrementa uno
+++contador ; //incrementa el valor de contador y retorna el valor
+
+mult *= 2; //Multiplica mult por dos, asigna ese valor a mult y retorna ese valor
+
+//TIPOS DE STATEMENTS
+//Declaration Statements
+var prueba; //se declara la variable prueba
+var daniel; //se declara la variable daniel
+
+function suma(a,b); //se declara la función suma
+
+
+
+
+
+
+
+
+
